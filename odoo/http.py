61bbe58e1a921771c613589adb6c7e5ef9b6bfb2
Martin Trigaux <mat@odoo.com>
2018-07-18 10:32:42 +0200

    Cherry-pick from 4e4dd4dfa2b898c064df93199f882c3829574d44

--- odoo/http.py
+++ odoo/http.py
@@ -1496,6 +1496,7 @@ def db_filter(dbs, httprequest=None):
     d, _, r = h.partition('.')
     if d == "www" and r:
         d = r.partition('.')[0]
+    d, h = re.escape(d), re.escape(h)
     r = odoo.tools.config['dbfilter'].replace('%h', h).replace('%d', d)
     dbs = [i for i in dbs if re.match(r, i)]
     return dbs
