113f4f6fcdec038882a9017fd6b60b3d3c08d046
Simon <simon.schmid@braintec-group.com>
2022-02-21 12:18:30 +0100

    [FIX] web: expect explicit sign up parameters
    Using an explicit list of sign up parameters will avoid
    polluting the context with unrelated values, and make
    debugging easier.
    
    (cherry picked from commit b08301e544263436e09762b0b252c96e732d14d0)

--- addons/web/controllers/main.py
+++ addons/web/controllers/main.py
@@ -73,6 +73,11 @@ db_list = http.db_list
 
 db_monodb = http.db_monodb
 
+# Shared parameters for all login/signup flows
+SIGN_UP_REQUEST_PARAMS = {'db', 'login', 'debug', 'token', 'message', 'error', 'scope', 'mode',
+                          'redirect', 'redirect_hostname', 'email', 'name', 'partner_id',
+                          'password', 'confirm_password', 'city', 'country_id', 'lang'}
+
 def serialize_exception(f):
     @functools.wraps(f)
     def wrap(*args, **kwargs):
@@ -478,7 +483,7 @@ class Home(http.Controller):
         if not request.uid:
             request.uid = odoo.SUPERUSER_ID
 
-        values = request.params.copy()
+        values = {k: v for k, v in request.params.items() if k in SIGN_UP_REQUEST_PARAMS}
         try:
             values['databases'] = http.db_list()
         except odoo.exceptions.AccessDenied:
