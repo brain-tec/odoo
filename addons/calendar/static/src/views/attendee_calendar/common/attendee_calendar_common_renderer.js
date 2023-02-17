a5dc52595ee03cd3656990ca04c73a2b8baf45a2
Florian Damhaut <flda@odoo.com>
2023-02-17 08:24:32 +0000

    [IMP] calendar: Make calendar edit more restricted
    This task change the people who can edit a calendar event.
    This calendar event currently allow every user to modify every
    event in the calendar. This cause issue where one user can
    modify the time-off event for other employees.
    
    This PR change the right to modify an event from the calendar
    view in the calendar app. With these changes, only the people
    attending an event will be able to modify it from the calendar
    view.
    
    task-id : 3185743
    
    closes odoo/odoo#112964
    
    Signed-off-by: Arnaud Joset <arj@odoo.com>

--- addons/calendar/static/src/views/attendee_calendar/common/attendee_calendar_common_renderer.js
+++ addons/calendar/static/src/views/attendee_calendar/common/attendee_calendar_common_renderer.js
@@ -1,9 +1,15 @@
 /** @odoo-module **/
 
+import { useService } from "@web/core/utils/hooks";
 import { CalendarCommonRenderer } from "@web/views/calendar/calendar_common/calendar_common_renderer";
 import { AttendeeCalendarCommonPopover } from "@calendar/views/attendee_calendar/common/attendee_calendar_common_popover";
 
 export class AttendeeCalendarCommonRenderer extends CalendarCommonRenderer {
+
+    setup() {
+        super.setup();
+        this.user = useService("user");
+    }
     /**
      * @override
      *
@@ -13,6 +19,7 @@ export class AttendeeCalendarCommonRenderer extends CalendarCommonRenderer {
         return {
             ...super.convertRecordToEvent(record),
             id: record._recordId || record.id,
+            editable: record.rawRecord.partner_ids.includes(this.user.partnerId),
         };
     }
 
